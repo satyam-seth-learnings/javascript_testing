fe388886f6f6726c6a89785d0f4d45f151a098c3
satyam-seth <satyam1998.1998@gmail.com>
2022-08-04 21:47:06 +0530

    6th commit

---
+++ QA Box Let's Test/6.Command Line Options - Parallel Tests, Bail, Different Reports, Test Filters/README.md
@@ -0,0 +1,1 @@
+<img width="1117" alt="image" src="https://user-images.githubusercontent.com/63374020/182882171-c81a7c94-a059-420b-a5ee-ab4e99128487.png">
---
+++ QA Box Let's Test/6.Command Line Options - Parallel Tests, Bail, Different Reports, Test Filters/test/async.app.spec.js
@@ -0,0 +1,75 @@
+const expect = require('chai').expect;
+const axios = require('axios');
+
+// describe.only('Async test suite', function () {
+// describe.skip('Async test suite', function () {
+describe('Async test suite', function () {
+    this.retries(3);
+    let count = 0;
+
+    // it.skip('Promised based way', function () {
+    it('Promised based way', function () {
+        this.timeout(10);
+
+        console.log('Retry Attempt#', count);
+        count++;
+        this.retries(5);
+
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.int');
+        })
+    })
+
+    it('done based way', (done) => {
+        axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+            done();
+        }).catch(err => {
+            done(err);
+        })
+    })
+
+    it('await based way', async () => {
+        const res = await axios.get('https://reqres.in/api/users/2')
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+    })
+
+    it('Test 2', () => {
+        console.log('test 2');
+    })
+})
+
+describe('2 - async test suite', function () {
+    this.retries(3);
+    let count = 0;
+
+    it('Promised based way', function () {
+        this.timeout(10);
+
+        console.log('Retry Attempt#', count);
+        count++;
+        this.retries(5);
+
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.int');
+        })
+    })
+
+    it('done based way', (done) => {
+        axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+            done();
+        }).catch(err => {
+            done(err);
+        })
+    })
+
+    it('async await based way', async () => {
+        const res = await axios.get('https://reqres.in/api/users/2')
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+    })
+
+    it('Test 2', () => {
+        console.log('test 2');
+    })
+})
